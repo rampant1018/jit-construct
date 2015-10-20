0691e9ce86e5d23d388fca9f8cf6f0a19e09443c
Jeff Liaw <rampant1018@gmail.com>
2015-10-20 17:42:36 +0800

    [bench] Add benchmark target.
    Add 4 extra target:
      * progs/sierpinski.b
      * progs/hello.b
      * progs/oobrain.b
      * progs/test.b

--- tests/bench.py
+++ tests/bench.py
@@ -15,6 +15,10 @@ def get_output(program, stdin):
 expected_output_hashes = {
     'progs/mandelbrot.b': 'b77a017f811831f0b74e0d69c08b78e620dbda2b',
     'progs/hanoi.b': '32cdfe329039ce63531dcd4b340df269d4fd8f7f',
+    'progs/sierpinski.b': 'b08c96af246127d95cad5e9f261d227fb685109b',
+    'progs/hello.b': 'a0b65939670bc2c010f4d5d6a0b3e4e4590fb92b',
+    'progs/oobrain.b': '69641b149daa97a7a6c0f8bff9566ffe38b75258',
+    'progs/test.b': 'b6589fc6ab0dc82cf12099d1c2d40ab994e8410c',
     ('progs/awib.b', open('progs/awib.b').read()): '3b4f9a78ec3ee32e05969e108916a4affa0c2bba'
 }
 
